62bd4d2d321eda4dbe4c59fac960ab095d6911f4
Myeongki Jeong <bright.jung@gmail.com>
2015-04-18 18:10:23 +0900

    modified:   .meteor/packages 	modified:   .meteor/versions 	modified:   client/templates/posts/posts_list.js 	client/main.js 	lib/ 	server/

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,22 +1,6 @@
-var postsData = [
-    {
-        title: 'Introduce Telescope',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://metero.com'
-    },
-    {
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    },
-    {
-        title: '공주대학교',
-        url: 'http://www.kongju.ac.kr'
-    }
-];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function()  {
+    	return Posts.find();
+    }
 });
 
